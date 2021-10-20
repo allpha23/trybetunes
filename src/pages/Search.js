b6df30b3d817b38a5fc833e68c828726c105bafe
allpha23 <allpha23@gmail.com>
2021-10-20 20:41:05 -0300

    Requisito 5 finalizado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,45 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      min: 2,
+      name: '',
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(e) {
+    const { value, name } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { min, name } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <label htmlFor="input-search">
+          <input
+            onChange={ this.onInputChange }
+            data-testid="search-artist-input"
+            id="input-search"
+            type="text"
+            name="name"
+          />
+        </label>
+        <input
+          disabled={ name.length < min }
+          data-testid="search-artist-button"
+          type="submit"
+          value="Pesquisar"
+        />
       </div>
     );
   }
